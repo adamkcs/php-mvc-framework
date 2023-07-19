3c59ecf24fe1c3fef58bc684e0dc9ceecce490ec
cxrqt1 <42243421+cxrqt1@users.noreply.github.com>
2023-07-19 20:35:09 +0100

    modify gitignore params

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'adamkocsis/php-mvc-framework',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bdcd5958efcf742401ea029aa57a98fd999dcc11',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'adamkocsis/php-mvc-framework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bdcd5958efcf742401ea029aa57a98fd999dcc11',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
